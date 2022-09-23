3dd37b38fd5dd4ffc316d0f448e59cddaf4b02c7
KEA <lena_konovalova@rambler.ru>
2022-09-23 18:36:11 +0300

    z68

---
+++ ceminar_9/задача_68/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine());
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    
+    else if ((m != 0) && (n == 0)) return Akkerman(m - 1, 1);
+   
+    else return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    
+}
+Console.WriteLine(Akkerman(m,n));
+
